58a00553924c94cd8b5482556b9742f8689a9140
David Ramirez <ramirezmonroyd@gmail.com>
2023-02-27 20:58:49 -0500

    style: ♻️ refactor main.py uri

--- watchguardapi/main.py
+++ watchguardapi/main.py
@@ -4,16 +4,21 @@ from riskmodel.routecrosses import route_cross_cat, route_cross_upz
 
 app = FastAPI()
 
-@app.get("/watchguard/api/requestroute/{mode}/{source_latitude}/{source_longitude}/{target_latitude}/{target_longitude}/{gender}/{hour}/{day}/{month}")
-async def get_endpoint(mode: str, source_latitude: float,source_longitude: float,
-                       target_latitude: float,target_longitude: float, 
-                       gender: str, hour: str, day: str, month: str ):
-    route = Route (mode, source_latitude, source_longitude, 
-                   target_latitude, target_longitude, 
-                   gender, hour, day, month)
+@app.get("/routes/{mode}/")
+async def get_endpoint(
+        mode: str, source_latitude: float,source_longitude: float,
+        target_latitude: float,target_longitude: float, 
+        gender: str, hour: str, day: str, month: str 
+):
+    route = Route (
+        mode, source_latitude, source_longitude, 
+        target_latitude, target_longitude, 
+        gender, hour, day, month
+    )
     response = route.get_response()
-    #print(route_cross_cat(route.get_shape(response.json())))
-    res = route_cross_upz(route.get_shape(response.json()), mode, gender, hour)
+    routes_shapes = route.get_shape(response.json())
+    res = route_cross_upz(routes=routes_shapes, mode=mode, gender=gender, hour=hour)
     for index, value in enumerate(res):
         print(f'🔥 {index} - {value}\n')
+        
     return route.get_response().json()
