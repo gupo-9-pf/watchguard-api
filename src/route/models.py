7f4110ff9f00104b1cc7990ca8ed6281f6110024
David Ramirez <ramirezmonroyd@gmail.com>
2023-02-28 01:24:33 -0500

    refactor!: ♻️ move route's logic and models to its own folder
    Follow a new structure that allows more scalability for the future where all the modules related to models or routes rest in an unique folder

---
+++ src/route/models.py
@@ -0,0 +1,25 @@
+import requests
+from dataclasses import dataclass
+from requests import Response
+from shapely.geometry import LineString
+
+
+@dataclass
+class Route:
+    mode: str
+    source_latitude: float
+    source_longitude: float
+    target_latitude: float
+    target_longitude: float
+    gender: str
+    hour: str
+    day: str
+    month: str
+
+    def get_shape(self, response: dict) -> LineString:
+        return [LineString(item['geometry']['coordinates'])
+                for item in response.get('routes')]
+
+    def get_response(self) -> Response:
+        url: str = f'https://routing.openstreetmap.de/routed-{self.mode}/route/v1/driving/{self.source_longitude},{self.source_latitude};{self.target_longitude},{self.target_latitude}?alternatives=true&overview=full&steps=true&geometries=geojson'
+        return requests.get(url=url)
